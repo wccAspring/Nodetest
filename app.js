fcb9fbff8e1e4e4102572a010d64ae92f55b4408
wangchengchun <40619581+wccAspring@users.noreply.github.com>
2020-09-09 09:26:16 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var bodyParser=require('body-parser');
+
+
+var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+//设置跨域请求
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
+
+// session
+var session = require('express-session');
+app.use(session({
+  secret: 'classweb0731', // 设置session签名
+  name: 'classweb',
+  cookie: {maxAge: 60 * 1000 * 60 * 24}, // 存储时间 24 小时
+  resave: false, // 每次请求都重新设置session
+  saveUninitialized: true
+}))
+
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//用户管理页面
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+module.exports = app;
